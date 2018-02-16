90125c2209ccee1df7ff0b2720299ede080c2fe8
Faris Afif <muhfaris@disroot.org>
2018-02-16 08:06:29 +0700

    remove

--- basic_serialdata/05_graphql/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"github.com/muhfaris/goFun/basic_serialdata/05_graphql/cmd"
-)
-
-func main() {
-	cmd.Execute()
-}
--- basic_serialdata/05_graphql/pkg/resolvers/hello_resolver.go
+++
@@ -1,7 +0,0 @@
-package Resolvers
-
-type Resolvers struct{}
-
-func (r *Resolvers) Hello() string {
-	return "Hello Graphql"
-}
